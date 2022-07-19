0787ec94a416e03fc82a36cf76513780f579de99
Agustín Vega <agustinvegadev@gmail.com>
2022-07-18 22:09:06 -0300

    Improved the code of getMoonAnimations helper

--- src/helper/geMoonAnimations.ts
+++ src/helper/geMoonAnimations.ts
@@ -1,6 +1,6 @@
 import { useActiveMoon } from "../hooks";
 
-export const getMoonAnimation = (side: string, animateInitial: boolean) => {
+export const getMoonAnimation = (side: string, noAnimateInitial: boolean) => {
 
     const { activeMoon } = useActiveMoon();
 
@@ -9,7 +9,7 @@ export const getMoonAnimation = (side: string, animateInitial: boolean) => {
     switch (side) {
         case 'top':
             animations = {
-                initial: { position: 'fixed', top: !animateInitial ? -300 : -175, right: 0, left: 0, margin: 'auto', },
+                initial: { position: 'fixed', top: (noAnimateInitial ? -175 : -300), right: 0, left: 0, margin: 'auto', },
                 animate: { top: -175 },
                 exit: activeMoon === 'top' ? { rotate: 360, y: window.innerHeight + 100 } : { top: -300 },
             }
@@ -17,7 +17,7 @@ export const getMoonAnimation = (side: string, animateInitial: boolean) => {
 
         case 'bottom':
             animations = {
-                initial: { position: 'fixed', bottom: !animateInitial ? -300 : -175, right: 0, left: 0, margin: 'auto', },
+                initial: { position: 'fixed', bottom: (noAnimateInitial ? -175 : -300), right: 0, left: 0, margin: 'auto', },
                 animate: { bottom: -175 },
                 exit: activeMoon === 'bottom' ? { rotate: 360, y: -window.innerHeight - 100 } : { bottom: -300 },
             }
@@ -25,7 +25,7 @@ export const getMoonAnimation = (side: string, animateInitial: boolean) => {
 
         case 'right':
             animations = {
-                initial: { position: 'fixed', right: !animateInitial ? -300 : -175, top: 0, bottom: 0, margin: 'auto', },
+                initial: { position: 'fixed', right: (noAnimateInitial ? -175 : -300), top: 0, bottom: 0, margin: 'auto', },
                 animate: { right: -175 },
                 exit: activeMoon === 'right' ? { rotate: 360, x: -window.innerWidth - 100 } : { right: -300 },
             }
@@ -33,7 +33,7 @@ export const getMoonAnimation = (side: string, animateInitial: boolean) => {
 
         case 'left':
             animations = {
-                initial: { position: 'fixed', left: !animateInitial ? -300 : -175, top: 0, bottom: 0, margin: 'auto', },
+                initial: { position: 'fixed', left: (noAnimateInitial ? -175 : -300), top: 0, bottom: 0, margin: 'auto', },
                 animate: { left: -175 },
                 exit: activeMoon === 'left' ? { rotate: 360, x: window.innerWidth + 100 } : { left: -300 },
             }
